686ed3ba89ee0f18d5c5be76f85f6359053b41e2
Waleed Alhrbi <waleedalhrbi2001@gmail.com>
2025-04-21 15:03:06 +0300

    Initial commit

---
+++ src/main/java/com/example/warehouseplatform/WarehousePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.warehouseplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehousePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehousePlatformApplication.class, args);
+    }
+
+}
